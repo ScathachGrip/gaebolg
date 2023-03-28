7d7179bc353762107afd33f0f9a0e59c1448b56a
sinkaroid <bibsindra@gmail.com>
2023-03-28 16:58:51 +0700

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import { APIGatewayEvent } from "aws-lambda";
+import fetch from "node-fetch";
+import { successDelivered, errorNoParams, errorTagsParams } from "./utils/handler";
+import { iParams, valid_type,
+  valid_image_hentai, valid_image_porn, valid_image_nasuverse } from "./constant/data";
+
+export async function handler(
+  event: APIGatewayEvent) {
+
+  const userAgent = event.multiValueHeaders["user-agent"] ? event.multiValueHeaders["user-agent"][0] : "no user agent";
+  const gateway = { specs: event.queryStringParameters as unknown as iParams };
+
+  if (!valid_type.includes(gateway.specs.type)) return errorNoParams(userAgent);
+
+  else if (gateway.specs.type === "hentai"
+    && !valid_image_hentai.includes(gateway.specs.image)) return errorTagsParams("hentai");
+
+  else if (gateway.specs.type === "porn"
+    && !valid_image_porn.includes(gateway.specs.image)) return errorTagsParams("porn");
+
+  else if (gateway.specs.type === "nasuverse"
+    && !valid_image_nasuverse.includes(gateway.specs.image)) return errorTagsParams("nasuverse");
+
+  else {
+    try {
+      let baseUrl = "", image = "";
+      if (gateway.specs.type === "hentai") 
+        baseUrl = "https://melony.scathach.id", image = gateway.specs.image;
+      else if (gateway.specs.type === "porn") 
+        baseUrl = "https://tristan.scathach.id", image = gateway.specs.image;
+      else if (gateway.specs.type === "nasuverse") 
+        baseUrl = "https://emiya.scathach.id", image = gateway.specs.image;
+
+      // else return invalidParams(gateway.specs.type);
+
+      const response = await fetch(`${baseUrl}/${image}/hey.json`);
+      const res = await response.json();
+      const randomData = res[Math.floor(Math.random() * res.length)];
+      return successDelivered(randomData, userAgent);
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          success: false,
+          message: `An error occured: ${error.message}`,
+          user_agent: userAgent
+        }),
+      };
+      
+    }
+  }
+}
---
+++ src/utils/handler.ts
@@ -0,0 +1,71 @@
+import { valid_image_hentai, valid_image_porn, valid_image_nasuverse } from "../constant/data";
+import { version, name } from "../../package.json";
+
+
+export async function errorNoParams(ua: string) {
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      success: false,
+      version: `${name} ${version}`,
+      params: [
+        {
+          type: "hentai",
+          image: valid_image_hentai
+        },
+        {
+          type: "porn",
+          image: valid_image_porn
+        },
+        {
+          type: "nasuverse",
+          image: valid_image_nasuverse
+        }
+      ],
+      message: "You didn't provide any parameters or it's invalid",
+      message_again: "Type properties tells it all about nsfw returns or not",
+      user_agent: ua
+    }),
+  };
+}
+
+export async function invalidParams(genre: string) {
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: `No matching parameters for ${genre}`
+    }),
+  };
+}
+
+export async function errorTagsParams(genre: string) {
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: `Invalid parameters for ${genre}`
+    }),
+  };
+} 
+
+export async function maybeError(genre: string) {
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: `This parameter is not valid for ${genre}`
+    }),
+  };
+} 
+
+export async function successDelivered(url: string, userAgent: string) {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      success: true,
+      url: url,
+      user_agent: userAgent
+    })
+  };
+}
