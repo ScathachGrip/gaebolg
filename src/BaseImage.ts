48f5aecae9416b00fde68b84f221848c84276f1b
sinkaroid <bibsindra@gmail.com>
2023-04-12 21:30:21 +0700

    add BaseImage

--- src/BaseImage.ts
+++ src/BaseImage.ts
@@ -1,9 +1,10 @@
 export default {
-  TYPE: ["hentai", "porn", "cute", "nasuverse"],
+  TYPE: ["hentai", "porn", "cute", "nasuverse", "minigames"],
   HENTAI_IMAGE: ["3some", "69", "anal", "ass", "bdsm", "blowjob", "boobjob", "boobs", "cum", "dick_ride", 
     "facesit", "feet", "finger", "furry", "futa", "handjob", "hentai", "kuni", "neko", "pet", 
     "pussy", "sex", "solo", "spank", "strip", "tentacle", "toys", "wank", "yaoi", "yuri"],
   PORN_IMAGE: ["ass", "blowjob", "boobs", "cowgirl", "cum", "doggie", "pussy", "sex"],
   NASUVERSE_IMAGE: ["angry", "bonk", "cry", "happy", "hug", "kiss", "lol", "nom", "pat", "pout", "smug", "uwu", "wink"],
-  CUTE_IMAGE: ["baka", "bite", "blush", "comfy", "cry", "cuddle", "dance", "feed", "fluff", "grab", "handhold", "highfive", "hug", "kiss", "lick", "neko", "pat", "poke", "punch", "slap", "smile", "smug", "stare", "tail", "tickle", "wag", "wasted", "wave", "wink"]
+  CUTE_IMAGE: ["baka", "bite", "blush", "comfy", "cry", "cuddle", "dance", "feed", "fluff", "grab", "handhold", "highfive", "hug", "kiss", "lick", "neko", "pat", "poke", "punch", "slap", "smile", "smug", "stare", "tail", "tickle", "wag", "wasted", "wave", "wink"],
+  MINIGAMES_IMAGE: ["arknights", "azurlane", "fgo", "fire_emblem", "genshin_impact", "girls_frontline", "kancolle", "vtubers"]
 };
