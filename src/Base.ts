bf6f1323070b5b4878c12caa3de235aa10afd670
sinkaroid <bibsindra@gmail.com>
2023-04-12 21:30:03 +0700

    add base

--- src/Base.ts
+++ src/Base.ts
@@ -2,5 +2,6 @@ export default {
   HENTAI: "https://melony.scathach.id",
   PORN: "https://tristan.scathach.id",
   CUTE: "https://vlad.scathach.id",
-  NASUVERSE: "https://emiya.scathach.id"
+  NASUVERSE: "https://emiya.scathach.id",
+  MINIGAMES: "https://tomoe.scathach.id"
 };
